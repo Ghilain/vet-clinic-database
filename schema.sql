8832327a551c3800163ef9e5504a8da20a0b2d67
Ghilain <ishimweghilain5050@gmail.com>
2022-07-15 21:07:43 +0200

    Create database and table animals

---
+++ schema.sql
@@ -0,0 +1,10 @@
+/* Database schema to keep the structure of entire database. */
+
+CREATE TABLE animals(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(110) NOT NULL,
+    date_of_birth DATE,
+    escapee_attempts INT,
+    neutered BOOLEAN,
+    weight_kg FLOAT
+)

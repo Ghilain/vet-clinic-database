3e337c9a017c6feff2c07cee64733b631031b749
Ghilain <ishimweghilain5050@gmail.com>
2022-07-16 11:22:56 +0200

    create two table and modify animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
+-- Insert data in animals table
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escapee_attempts) VALUES ('Agumon', '2020-02-03', 10.23, true, 0);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escapee_attempts) VALUES ('Gabumon', '2018-11-15', 8, true, 2);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escapee_attempts) VALUES ('Pikachu', '2021-01-07', 15.04, false, 1);
--- schema.sql
+++ schema.sql
@@ -1,7 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
+-- create database vet_clinic
 CREATE DATABASE vet_clinic;
 
+-- create table animals
 CREATE TABLE animals(
     id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(110) NOT NULL,
@@ -14,3 +16,24 @@ CREATE TABLE animals(
 /* add new column*/
 
 ALTER TABLE animals ADD species VARCHAR(200);
+
+-- create table owners
+CREATE TABLE owners(
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(110) NOT NULL,
+    age INT
+);
+
+-- create table species
+CREATE TABLE species(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(110) NOT NULL
+);
+
+/* remove columns species in animals*/
+
+ALTER TABLE animals DROP species;
+
+/* add new columns in animals*/
+ALTER TABLE animals ADD species_id INT, ADD FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE CASCADE;
+ALTER TABLE animals ADD owner_id INT, ADD FOREIGN KEY (owner_id) REFERENCES owners(id) ON DELETE CASCADE;

76c5c27edde00ed6f08a22a42b7ea2c41aa25343
Ghilain <ishimweghilain5050@gmail.com>
2022-07-16 13:56:13 +0200

    updates

--- data.sql
+++ data.sql
@@ -21,7 +21,7 @@ INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
 INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
 INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
 
--- Insert data in table owners
+-- Insert data in table species
 INSERT INTO species (name) VALUES ('Pokemon');
 INSERT INTO species (name) VALUES ('Digimon');
 
